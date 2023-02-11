5fbcab27533b951775183621192d62f7c560982b
Guilherme D. Garcia <gdgarcia@gdgarcia-mac.local>
2023-02-11 18:30:41 -0500

    improved bigram fnc

--- R/biGram_pt.R
+++ R/biGram_pt.R
@@ -4,33 +4,31 @@
 #' @param word A possible string in Portuguese in its phonemic form without syllabification or stress. The only diacritic that should be used is the tilde for nasals, e.g., ã.
 #' @return The phonemic transcription for the string in question
 #' @examples
-#' ipa_pt(word = "palado");
-#' ipa_pt(word = "sɛtiko");
-#' ipa_pt(word = "mãw̃");
+#' biGram(word = "paklode")
 #' @export
 
 biGram_pt = function(word = ""){
 
-  if (!require("pacman")) install.packages("pacman")
+  if (!require("pacman", quietly = T)) install.packages("pacman")
   pacman::p_load(tidyverse)
 
-  corpus = pt_lex %>%
+  words = pt_lex %>%
     mutate(proB = str_remove_all(pro, "\\.|'")) %>%
     pull(proB)
 
-  word_split = str_split(word, pattern = "")[[1]]
+  x1 = str_split(word, pattern = "")[[1]]
 
   bigrams = c()
-  bigrams[1] = paste("^", word_split[1], sep = "")
+  bigrams[1] = paste("^", x1[1], sep = "")
 
   # Adding word-internal bigrams
-  for(i in 1:(length(word_split)-1)){
-    seq = str_c(word_split[i], word_split[i+1], sep = "")
+  for(i in 1:(length(x1)-1)){
+    seq = str_c(x1[i], x1[i+1], sep = "")
     bigrams[length(bigrams)+1] = seq
   }
 
   # Adding word-final bigram
-  bigrams[length(bigrams)+1] = str_c(word_split[length(word_split)], "$", sep = "")
+  bigrams[length(bigrams)+1] = str_c(x1[length(x1)], "$", sep = "")
 
   # Variable for all probabilities
   probs = c()
