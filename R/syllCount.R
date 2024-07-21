3824bb197bf6d2589216cf28f3b8b4607c3a7a0d
Guilherme D. Garcia <guilherme.garcia@lli.ulaval.ca>
2024-07-21 15:23:02 -0400

    new function added to count number of syllables of a given string

---
+++ R/syllCount.R
@@ -0,0 +1,18 @@
+#' Syllable counter
+#'
+#' The function counts the number of syllables in a given string
+#' @param word A possible string that is already syllabified
+#' @param sep The syllable boundary employed. By default, the
+#' function assumes a period separates syllables, but you may want to
+#' change that using the `sep` argument. Bear in mind that you must
+#' follow the typical syntax used in RegEx in R for special symbols
+#' @return The number of syllables in the string of interest
+#' @examples
+#' syllCount("kan.ˈto.zo")
+#' @export
+
+syllCount = function(word = "", sep = "\\."){
+  count = stringr::str_count(word, pattern = sep) + 1
+
+  return(count)
+}
