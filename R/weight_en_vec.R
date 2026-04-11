4493d264972722d526893721d942cf3031dcc338
Guilherme D. Garcia <guilherme.garcia@lli.ulaval.ca>
2026-04-11 01:10:22 -0400

    improved english and french weight systems

--- R/weight_en_vec.R
+++ R/weight_en_vec.R
@@ -6,10 +6,32 @@
 #' @noRd
 
 getWeight_en <- function(word = c("\u02c8h\u0251s.p\u026a.t\u0259l")) {
-  .getWeight_by_vowels(
-    word,
-    c("e\u026a", "o\u028a", "a\u028a", "\u0254\u026a", "a\u026a", "i", "u",
-      "\u025b", "\u026a", "\u0251", "\u028c", "\u00e6", "\u0259",
-      "\u025a", "\u028a", "\u0254")
-  )
+  syl_list <- word |>
+    stringr::str_split("\\.")
+
+  syl_list <- lapply(syl_list, function(syls) {
+    if (length(syls) == 1 && is.na(syls)) {
+      return(NA_character_)
+    }
+
+    vapply(syls, function(syl) {
+      if (is.na(syl)) return(NA_character_)
+
+      if (.is_heavy_syllable_ipa_en(syl)) {
+        "H"
+      } else {
+        "L"
+      }
+    }, USE.NAMES = FALSE, FUN.VALUE = character(1))
+  })
+
+  profile <- lapply(syl_list, function(x) rev(rev(x)[1:3]))
+  profile <- lapply(profile, function(x) x[!is.na(x)])
+
+  profile <- lapply(profile, function(x) stringr::str_c(x, collapse = "")) |>
+    unlist()
+
+  profile[profile %in% ""] <- NA
+
+  profile
 }
--- R/weight_fr_vec.R
+++ R/weight_fr_vec.R
@@ -7,5 +7,57 @@
 
 getWeight_fr <- function(word = c("\u025b\u0303.f\u0254\u0281.ma.tik")) {
   vowels <- get("vowels_fr", envir = as.environment("package:Fonology"))
-  .getWeight_by_vowels(word, vowels)
+  heavy_vowels <- c(
+    "\u0251", "e", "\u00f8", "o",
+    "\u025b\u0303", "\u0153\u0303", "\u0254\u0303", "\u0251\u0303"
+  )
+  vowel_extract_pattern <- paste0(
+    "(",
+    paste(vowels[order(nchar(vowels), decreasing = TRUE)], collapse = "|"),
+    ")"
+  )
+  vowels_pattern <- .build_vowel_pattern(vowels)
+
+  syl_list <- word |>
+    stringr::str_split("\\.")
+
+  syl_list <- lapply(syl_list, function(syls) {
+    if (length(syls) == 1 && is.na(syls)) {
+      return(NA_character_)
+    }
+
+    n_syl <- length(syls)
+
+    vapply(seq_along(syls), function(i) {
+      syl <- syls[i]
+
+      if (is.na(syl)) return(NA_character_)
+
+      syl_clean <- stringr::str_remove_all(syl, "[\u02c8\u02cc]")
+      nucleus <- stringr::str_extract(syl_clean, vowel_extract_pattern)
+
+      if (is.na(nucleus)) {
+        return(NA_character_)
+      }
+
+      has_coda <- !stringr::str_detect(syl_clean, vowels_pattern)
+      open_final <- i == n_syl && !has_coda
+
+      if (has_coda || (!open_final && nucleus %in% heavy_vowels)) {
+        "H"
+      } else {
+        "L"
+      }
+    }, USE.NAMES = FALSE, FUN.VALUE = character(1))
+  })
+
+  profile <- lapply(syl_list, function(x) rev(rev(x)[1:3]))
+  profile <- lapply(profile, function(x) x[!is.na(x)])
+
+  profile <- lapply(profile, function(x) stringr::str_c(x, collapse = "")) |>
+    unlist()
+
+  profile[profile %in% ""] <- NA
+
+  profile
 }
